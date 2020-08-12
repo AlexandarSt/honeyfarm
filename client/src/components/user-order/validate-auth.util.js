7918d4b1b0cc172c1f7f0bd48f7be16739ec9d80
Aleksandar Stojkovic <alexstweb@gmail.com>
2020-08-12 14:28:36 +0200

    Dodat regex na strani za narucivanje

--- client/src/components/user-order/validate-auth.util.js
+++ client/src/components/user-order/validate-auth.util.js
@@ -1,15 +1,21 @@
 export const validateAuth = (values) => {
         const errors = {};
         if (!values.firstName) {
-            errors.firstName = 'Unesite vaše ime';
+          errors.firstName = 'Unesite vaše ime';
+        } else if (!/^[a-zA-Z]+$/.test(values.firstName)) {
+          errors.firstName = 'Možete uneti samo slova';
         } else if (!values.lastName) {
           errors.lastName = 'Unesite vaše prezime';
+        } else if (!/^[a-zA-Z]+$/.test(values.lastName)) {
+          errors.lastName = 'Možete uneti samo slova';
         } else if (!values.address) {
           errors.address = 'Unesite vašu adresu';
         } else if (!values.city) {
           errors.city = 'Unesite vaš grad';
         } else if (!values.phone) {
           errors.phone = 'Unesite vaš kontakt telefon';
+        } else if (!/^[0-9\b]+$/.test(values.phone)) {
+          errors.phone = 'Unesite samo cifre broja bez razmaka';
         } else if (!values.email) {
             errors.email = 'Unesite e-mail adresu';
           } else if (
